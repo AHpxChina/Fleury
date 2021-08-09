f47a96ba2e421388f3d059f368324a2de7ebf119
AHpx <AHpx@yandex.com>
2021-08-09 21:37:07 +0800

    Initialized

---
+++ Fleury.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fleury.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Fleury.UnitTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace Fleury.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
